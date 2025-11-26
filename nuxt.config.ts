8fde12a1280e316389ebf6d12f1d8539c418dda4
Gabimaru <kraiwitsudnaen@gmail.com>
2025-11-27 00:27:59 +0700

    Edit icon web

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Games Play',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      ],
+    },
+  },
   compatibilityDate: '2025-07-15',
   modules: ['@nuxtjs/tailwindcss'],
   css: [
